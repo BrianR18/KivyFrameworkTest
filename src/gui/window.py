ec30b5769f5567c33c181da404ff685cc64570a0
Brian <briomero76@gmail.com>
2023-07-28 15:26:46 -0500

    Added operators

--- src/gui/window.py
+++ src/gui/window.py
@@ -13,6 +13,7 @@ class Window(BoxLayout):
         self.add_widget(self.__screen)
         self.add_widget(self.__keyboard)
         self.__bindEventsOnButtons()
+        self.keyboard.buttons['del'].bind(on_press=self.deleteNumberFromScreen)
 
     @property
     def screen(self):
@@ -23,12 +24,21 @@ class Window(BoxLayout):
         return self.__keyboard
 
     def writeOnScreen(self, instance):
+        signs = ['+', '-', '*', '/']
         screenText = self.__screen.text
-        screenText += instance.text
+        lastChar = screenText[len(screenText)-1:]
+        if not (instance.text in signs and lastChar in signs):
+            screenText += instance.text
+        self.__screen.text = screenText
+
+    def deleteNumberFromScreen(self, instance):
+        screenText = self.__screen.text
+        screenText = screenText[:len(screenText)-1]
         self.__screen.text = screenText
 
     def __bindEventsOnButtons(self):
-        number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.']
+        number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-',
+                  '*', '/']
         for btn in self.keyboard.buttons:
             if btn in number:
                 self.keyboard.buttons[btn].bind(on_press=self.writeOnScreen)
